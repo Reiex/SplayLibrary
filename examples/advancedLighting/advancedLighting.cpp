bb358a055f052ff1cb817f551329b39b35ba7d00
Reiex <reiex.marius38@gmail.com>
2022-03-24 22:24:24 +0100

    Clean example

--- examples/advancedLighting/advancedLighting.cpp
+++ examples/advancedLighting/advancedLighting.cpp
@@ -1,15 +1,15 @@
 #include "../main.hpp"
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
-
 int advancedLightingMain()
 {
 	spl::RenderWindow window({ 1000, 600 }, "SPL Example");
 
-	spl::Shader shader("examples/advancedLighting/resources/shaders/firstPass.vert", "examples/advancedLighting/resources/shaders/firstPass.frag");
+	spl::Framebuffer framebuffer;
+	framebuffer.createNewTextureAttachment<spl::Texture2D>(spl::FramebufferAttachment::ColorAttachment0, spl::uvec2{ 1000, 600 });
+	framebuffer.createNewRenderBufferAttachment(spl::FramebufferAttachment::DepthAttachment, spl::TextureInternalFormat::Depth24_Stencil8, spl::uvec2{ 1000, 600 });
+
+	spl::Shader shader1("examples/advancedLighting/resources/shaders/firstPass.vert", "examples/advancedLighting/resources/shaders/firstPass.frag");
+	spl::Shader shader2("examples/advancedLighting/resources/shaders/secondPass.vert", "examples/advancedLighting/resources/shaders/secondPass.frag");
 
 	spl::PerspectiveCamera camera({ 1000, 600 }, 0.1f, 100.f, 1.f);
 	camera.setTranslation({ 0.f, 0.f, 2.f });
@@ -19,11 +19,6 @@ int advancedLightingMain()
 	meshTransform.scale(0.01f);
 
 	spl::vec3 lightDir = -spl::normalize(spl::vec3{ 1.0, 0.0, 1.0 });
-
-	spl::Framebuffer framebuffer;
-
-	framebuffer.createNewTextureAttachment<spl::Texture2D>(spl::FramebufferAttachment::ColorAttachment0, spl::uvec2{ 1000, 600 });
-	framebuffer.createNewRenderBufferAttachment(spl::FramebufferAttachment::DepthAttachment, spl::TextureInternalFormat::Depth24_Stencil8, spl::uvec2{ 1000, 600 });
 
 	spl::Mesh<> screen(
 		{
@@ -35,9 +30,7 @@ int advancedLightingMain()
 		{
 			0, 1, 3, 1, 2, 3
 		}
-		);
-
-	spl::Shader shader2("examples/advancedLighting/resources/shaders/secondPass.vert", "examples/advancedLighting/resources/shaders/secondPass.frag");
+	);
 
 	while (!window.shouldClose())
 	{
@@ -65,13 +58,13 @@ int advancedLightingMain()
 
 		meshTransform.rotate({ -0.5f, 1.f, 0.3f }, 0.01f);
 
-		shader.use();
-		shader.setUniform("cameraPos", camera.getTranslation());
-		shader.setUniform("lightDir", lightDir);
+		shader1.use();
+		shader1.setUniform("cameraPos", camera.getTranslation());
+		shader1.setUniform("lightDir", lightDir);
 
-		shader.setUniform("projection", camera.getProjectionMatrix());
-		shader.setUniform("view", camera.getViewMatrix());
-		shader.setUniform("model", meshTransform.getTransformMatrix());
+		shader1.setUniform("projection", camera.getProjectionMatrix());
+		shader1.setUniform("view", camera.getViewMatrix());
+		shader1.setUniform("model", meshTransform.getTransformMatrix());
 
 		mesh.draw();
 
--- examples/main.cpp
+++ examples/main.cpp
@@ -1,13 +1,11 @@
 #include "main.hpp"
 
-#include <thread>
-
 int main()
 {
-	std::thread basicPhongThread(&basicPhongMain);
+	// std::thread basicPhongThread(&basicPhongMain);
 	std::thread advancedLightingThread(&advancedLightingMain);
 
-	basicPhongThread.join();
+	// basicPhongThread.join();
 	advancedLightingThread.join();
 
 	return 0;
